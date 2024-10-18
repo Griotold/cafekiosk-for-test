8618cad4e97ccb87af88f1e0c62600f8b2b645d6
Griotold <gotjd9773@naver.com>
2024-10-18 12:20:28 +0900

    [6-6] Presentation Layer 테스트 (1)

--- src/main/java/sample/cafekiosk/spring/api/controller/product/productController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/productController.java
@@ -2,19 +2,29 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 
 import java.util.List;
 
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @RestController
 public class productController {
 
     private final ProductService productService;
 
-    @GetMapping("/api/v1/products/selling")
+    @PostMapping("/new")
+    public void createProduct(ProductCreateRequest request) {
+        productService.createProduct(request);
+
+    }
+
+    @GetMapping("/selling")
     public List<ProductResponse> getSellingProducts() {
         return productService.getSellingProducts();
     }
--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -10,11 +12,32 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장 X, 변경감지 X(성능 향상)
+ * <p>
+ * CQRS - 서비스를 Command 용, Query 용으로 나누자
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    // 동시성 이슈 해결하는 방법
+    // 1. productNumber 에 유니크 인덱스를 걸고 재시도하는 로직을 추가하는 방법
+    // 2. UUID
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -22,5 +45,18 @@ public class ProductService {
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009
+        return String.format("%03d", nextProductNumberInt);
     }
 }
