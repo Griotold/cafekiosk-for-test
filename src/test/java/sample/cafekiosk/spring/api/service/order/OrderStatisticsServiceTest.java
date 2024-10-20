4ffb128e5588178dfa935d03c413480d474ce348
Griotold <gotjd9773@naver.com>
2024-10-20 13:17:00 +0900

    [7-1] Mockito로 Stubbing 하기 - todo 1. 파사드, 2. 이벤트 발행

---
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
@@ -0,0 +1,112 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.order.OrderStatus;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+@SpringBootTest
+class OrderStatisticsServiceTest {
+
+    @Autowired
+    private OrderStatisticsService orderStatisticsService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @MockBean
+    private MailSendClient mailSendClient;
+
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        mailSendHistoryRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("")
+    @Test
+    void sendOrderStatisticsMail() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2024, 10, 20, 0, 0, 0);
+
+        Product product1 = creatProduct(HANDMADE, "001", 1000);
+        Product product2 = creatProduct(HANDMADE, "002", 3000);
+        Product product3 = creatProduct(HANDMADE, "003", 5000);
+        List<Product> products = List.of(product1, product2, product3);
+        productRepository.saveAll(products);
+
+        Order order1 = createPaymentCompletedOrder(now.minusSeconds(1), products);
+        Order order2 = createPaymentCompletedOrder(now, products);
+        Order order3 = createPaymentCompletedOrder(now.plusDays(1).minusSeconds(1), products);
+        Order order4 = createPaymentCompletedOrder(now.plusDays(1), products);
+
+        // stubbing
+        when(mailSendClient.sendMail(any(String.class), any(String.class), any(String.class), any(String.class)))
+                .thenReturn(true);
+
+        // when
+        boolean result = orderStatisticsService.sendOrderStatisticsMail(now.toLocalDate(), "test@email.com");
+
+        // then
+        assertThat(result).isTrue();
+
+        List<MailSendHistory> histories = mailSendHistoryRepository.findAll();
+        assertThat(histories).hasSize(1)
+                .extracting("content")
+                .contains("총 매출 합계는 18000원입니다.");
+    }
+
+    private Order createPaymentCompletedOrder(LocalDateTime now, List<Product> products) {
+        Order order = Order.builder()
+                .products(products)
+                .orderStatus(OrderStatus.PAYMENT_COMPLETED)
+                .registeredDateTime(now)
+                .build();
+        return orderRepository.save(order);
+    }
+
+    private Product creatProduct(ProductType type, String productNumber, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(type)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("제품명")
+                .price(price)
+                .build();
+    }
+}
---
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderRepositoryTest.java
@@ -0,0 +1,87 @@
+package sample.cafekiosk.spring.domain.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+@ActiveProfiles("test")
+//@SpringBootTest
+@DataJpaTest
+class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("주문 상태와 날짜를 조건으로 주문을 조회한다.")
+    @Test
+    void findOrdersBy() {
+        // given
+        OrderStatus targetStatus = OrderStatus.PAYMENT_COMPLETED;
+        LocalDateTime targetOrderDateTime = LocalDateTime.of(2024, 10, 10, 0, 0,0);
+        Order order1 = createOrder(targetOrderDateTime, targetStatus);
+        Order order2 = createOrder(targetOrderDateTime, targetStatus);
+
+        Order order3 = createOrder(targetOrderDateTime.plusDays(1), targetStatus);
+        Order order4 = createOrder(targetOrderDateTime.plusDays(2), targetStatus);
+
+        Order order5 = createOrder(targetOrderDateTime, OrderStatus.INIT);
+        Order order6 = createOrder(targetOrderDateTime, OrderStatus.INIT);
+        orderRepository.saveAll(List.of(order1, order2, order3, order4, order5, order6));
+
+        LocalDateTime startDateTime = targetOrderDateTime;
+        LocalDateTime endDateTime = targetOrderDateTime.plusDays(1);
+
+        // when
+        List<Order> result = orderRepository.findOrdersBy(startDateTime, endDateTime, targetStatus);
+
+        // then
+        assertThat(result).hasSize(2)
+                .extracting("totalPrice", "orderStatus")
+                .containsExactlyInAnyOrder(
+                        tuple(15_500, targetStatus),
+                        tuple(15_500, targetStatus)
+                );
+    }
+
+    private Order createOrder(LocalDateTime orderDateTime, OrderStatus orderStatus) {
+        Product product1 = creatProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+        Product product2 = creatProduct("002", HANDMADE, HOLD, "카페라떼", 4500);
+        Product product3 = creatProduct("003", HANDMADE, STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        return Order.builder()
+                .products(List.of(product1, product2, product3))
+                .registeredDateTime(orderDateTime)
+                .orderStatus(orderStatus)
+                .build();
+    }
+
+    private Product creatProduct(String productNumber, ProductType type,
+                                 ProductSellingStatus sellingStatus, String name, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(type)
+                .sellingStatus(sellingStatus)
+                .name(name)
+                .price(price)
+                .build();
+    }
+
+}
